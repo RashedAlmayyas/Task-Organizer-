56b40e0f00b7529e175b66ab12645d8d024772d2
R-Almayyas <108932239+RashedAlmayyas@users.noreply.github.com>
2022-08-28 10:37:28 +0200

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,13 +1,39 @@
-function store() {
-  var name = document.getElementById("name");
-  var email = document.getElementById("email");
-  var password = document.getElementById("password");
-  var passwordConfirm = document.getElementById("passwordConfirm");
+const form = document.getElementById("form")
 
-  localStorage.setItem("name", name.value);
-  localStorage.setItem("email", email.value);
-  localStorage.setItem("password", password.value);
-  localStorage.setItem("passwordConfirm", passwordConfirm.value);
+let allUsers = [];
 
-  alert("Your account has been created");
+function User (fName,lName,email,password,passwordConf) {
+    this.fName = fName
+    this.lName = lName
+    this.email = email
+    this.password = password
+    this.passwordConf = passwordConf
 }
+
+function render (event) {
+    event.preventDefault();
+    console.log(event);
+
+    let firstName = event.target.fName.value;
+    let lastName = event.target.lName.value;
+    let uEmail = event.target.email.value;
+    let uPassword = event.target.password.value;
+    let uPasswordConf = event.target.passwordConfirm.value;
+    if(uPassword !== uPasswordConf){
+        alert('Passwords do not match');
+    }
+
+    let newUser = new User(firstName, lastName, uEmail,uPassword,uPasswordConf);
+    allUsers.push(newUser);
+
+    saveToLocal();
+
+    document.forms[0].reset();
+}
+function saveToLocal(){
+    let strArr = JSON.stringify(allUsers);
+    localStorage.setItem('allUsers',strArr);
+}
+
+
+form.addEventListener('submit', render);
